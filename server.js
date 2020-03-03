e2de301b89b86c0420dc1e0249ef00f973cde978
abdallah <ceo@kortby.com>
2020-03-03 12:52:48 -0800

    2nd commit

--- server.js
+++ server.js
@@ -25,27 +25,24 @@ app.get('/messages', (req, res) => {
   });
 });
 
-app.post('/messages', (req, res) => {
-  var message = new Message(req.body);
+app.post('/messages', async (req, res) => {
+  try {
+    var message = new Message(req.body);
+    var savedMessage = await message.save();
+    console.log('saved');
 
-  message
-    .save()
-    .then(() => {
-      console.log('saved');
-      return Message.findOne({ message: 'badword' });
-    })
-    .then(censored => {
-      if (censored) {
-        console.log('censored words found', censored);
-        return Message.remove({ _id: censored.id });
-      }
+    var censored = await Message.findOne({ message: 'badword' });
+    if (censored) {
+      await Message.remove({ _id: censored.id });
+    } else {
       io.emit('message', req.body);
-      res.sendStatus(200);
-    })
-    .catch(err => {
-      res.sendStatus(500);
-      return console.error(err);
-    });
+    }
+
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+    return console.error(error);
+  }
 });
 
 mongoose.connect(
